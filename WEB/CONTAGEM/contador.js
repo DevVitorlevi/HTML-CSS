f06ecc410a19a87b6d01768637d7e36ae2aa49c2
VT69 <160429477+Vt69xx@users.noreply.github.com>
2024-03-31 11:23:25 -0300

    main

---
+++ WEB/CONTAGEM/contador.js
@@ -0,0 +1,35 @@
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let pass = document.getElementById('txtp')
+    let res = document.querySelector('.res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || pass.value.length == 0){
+        res.innerHTML = 'Impossivel contar'
+        alert('[ERRO] FALTAM DADOS')
+    }
+    else {
+        res.innerHTML = 'Contando.. <br/> '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pass.value)
+        if (p <= 0 ){
+            alert('Passo Invalido ! Considerando PASSO 1')
+            p = 1
+
+        }
+        //contagem cresente 
+        if (i < f ){
+        for(let c = i; c <= f; c+=p){
+            res.innerHTML += ` ${c} \u{1f449}`
+        }
+    }
+    else{
+        //contagem regressiva
+        for(let c = i; c >= f; c-=p){
+            res.innerHTML += ` ${c} \u{1f449}`
+        }
+    }
+    res.innerHTML += ` \u{1f3c1} `
+    }
+}
