72721b5ea2e4c2963247efce33a0aa2ae1b4e811
Dev Vitor <160429477+DevVitorlevi@users.noreply.github.com>
2024-10-04 14:44:32 -0300

    att

---
+++ Form/script.js
@@ -0,0 +1,66 @@
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+const form = document.getElementById('form')
+const inputs = document.querySelectorAll('.required')
+const spans = document.querySelectorAll('.span-required')
+
+document.addEventListener('keydown',(evt)=>{
+    if(evt.key=='Enter'){
+        impedirEnvio(evt)
+    }
+})
+form.addEventListener('submit',impedirEnvio)
+function impedirEnvio(ev){
+    ev.preventDefault()//Impedir o Envio do Form
+    ValidarNome()
+    ValidarEmail()
+    ValidarSenha()
+    CompararSenha()
+}
+
+function CasoError(indice){
+    inputs[indice].style.border= '2px solid #e63636'
+    spans[indice].style.display='block'
+}
+function NotError(indice){
+    inputs[indice].style.border= ''
+    spans[indice].style.display='none'
+}
+
+inputs[0].addEventListener('input',ValidarNome)
+inputs[1].addEventListener('input',ValidarEmail)
+inputs[2].addEventListener('input',ValidarSenha)
+inputs[3].addEventListener('input',CompararSenha)
+
+function ValidarNome(){
+    if(inputs[0].value.length <3){
+        CasoError(0)
+    }else{
+        NotError(0)
+        
+    }
+}
+function ValidarEmail(){
+    if(emailRegex.test(inputs[1].value)){//Verificando se o E-mail é Válido
+    NotError(1)
+}else{
+    CasoError(1)
+}}
+
+function ValidarSenha(){
+    if(inputs[2].value.length <8){
+        CasoError(2)
+    }else{
+        NotError(2)  
+    }
+}
+
+function CompararSenha(){
+    if(inputs[2].value==inputs[3].value && inputs[3].value.length>=8){
+        NotError(3)
+    }else{
+        CasoError(3)
+    }
+}
+
+
+
